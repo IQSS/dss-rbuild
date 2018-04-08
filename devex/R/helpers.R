3bce7e015deaed1d37595b6d114028c5dbddc188
Asher Spector <amspector100@gmail.com>
2018-04-07 23:34:38 -0400

    test file: expectations

--- devex/R/helpers.R
+++ devex/R/helpers.R
@@ -26,6 +26,7 @@ loss <- function(x,y){
 #' \code{general_sqrt} returns the square root of any real number
 #'
 #' @param x A real number - integers or doubles are both acceptable.
+#' Vectors must be of length 1.
 #' @return A complex value. If x is positive, the imaginary component is equal to 0;
 #' if x is negative, the real component is equal to 0.
 #'
@@ -41,7 +42,7 @@ general_sqrt <- function (x){
 
   # Return the normal square root if x > 0
   if (x > 0 || x == 0){
-    return(complex(real=sqrt(x), imaginary=0))
+    return(complex(real = sqrt(x), imaginary = 0))
   }
 
   # Else return the complex square root
