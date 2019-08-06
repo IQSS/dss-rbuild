938bd294fe1003106b4060c3b647581a640e2835
stevenworthington <sworthington@iq.harvard.edu>
2019-08-06 15:14:59 -0400

    deploy

--- _deploy.sh
+++ _deploy.sh
@@ -9,11 +9,11 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "shirokuriwaki@gmail.com"
-git config --global user.name "Shiro Kuriwaki"
+git config --global user.email "sworthington@iq.harvard.edu"
+git config --global user.name "stevenworthington"
 
 # clone the repository to the book-deploy directory
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-deploy
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \book-deploy
 cd book-deploy
 git rm -rf *
 cp -r ../_book/* ./
