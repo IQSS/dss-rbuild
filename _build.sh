cc3bd7d04f3053914ddc147a8296d3ba0f0413d8
stevenworthington <sworthington@iq.harvard.edu>
2019-08-06 15:50:21 -0400

    deploy

--- _build.sh
+++ _build.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-# Called by .travis.yml. Compiles bookdown in HTML and pdf form. 
+# Called by .travis.yml. Compiles bookdown in HTML and pdf form.
 # All R packages needed to be listed in the separate DESCRIPTION file to be installed.
 
 set -ev
 
 Rscript -e "bookdown::render_book('rmd_files', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('rmd_files', 'bookdown::pdf_book')"
+
+# copy .nojekyll into docs directory
+cp .nojekyll docs/
+
+# remove temp .rds files
+rm *.rds
